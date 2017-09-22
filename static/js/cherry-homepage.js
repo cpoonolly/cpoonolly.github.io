2347143aa677b9067557bb8be85322604a08240d
Ubuntu <ubuntu@ip-172-31-60-155.ec2.internal>
2017-09-22 03:54:52 +0000

    Initial Commit

---
+++ static/js/cherry-homepage.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    console.log('welcome to cherry\'s homepage!');
+    $('.button-collapse').sideNav();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+});
