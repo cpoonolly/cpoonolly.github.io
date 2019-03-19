d1d4f39d9482f03b8c5621c56fb0248864dbb1ed
Cherry Poonolly <rpoonolly@gmail.com>
2019-03-20 02:51:37 +0700

    WIP - clearing everything out

--- static/js/cherry-homepage.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    console.log('welcome to cherry\'s homepage!');
-    $('.button-collapse').sideNav();
-    $('.carousel.carousel-slider').carousel({fullWidth: true});
-});
