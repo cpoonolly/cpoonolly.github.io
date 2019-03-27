388b7a8a86c9131513fbf49a083732e58f00e999
Cherry Poonolly <rpoonolly@gmail.com>
2019-03-27 22:19:33 +0700

    Adding about section + getting webpack to handle images plus other stuff

--- src/index.js
+++ src/index.js
@@ -1,2 +1,79 @@
 import 'cherry-webcomponents';
-import { Button } from "@material/mwc-button";
+// import { Button } from "@material/mwc-button";
+
+function onAboutMeBtnClick() {
+  bracketsSwitchSlot(1);
+  swarmGoCrazy();
+}
+
+function onExperienceBtnClick() {
+  bracketsSwitchSlot(2);
+  swarmGoCrazy();
+}
+
+function onProjectsBtnClick() {
+  bracketsSwitchSlot(3);
+  swarmGoCrazy();
+}
+
+function bracketsSwitchSlot(slotNum) {
+  document.getElementById('cherry-example-brackets').setAttribute('currentSlot', slotNum);
+}
+
+function swarmGoCrazy() {
+  console.log('swarmOrbitElement');
+  let cherrySwarmEl = document.getElementById('cherry-bg-swarm');
+
+  let swarmElRect = document.getElementById('cherry-bg-swarm').getBoundingClientRect();
+  let swarmElX = ((swarmElRect.right - swarmElRect.left) / 2) + swarmElRect.left;
+  let swarmElY = ((swarmElRect.bottom - swarmElRect.top) / 2) + swarmElRect.top;
+
+  let animationSequence = [];
+
+  // first freeze .1s
+  animationSequence.push({
+      duration: 100,
+      animationProps: {name: 'freeze'}
+  });
+
+  // then go grazy for 1s
+  animationSequence.push({
+      duration: 1000,
+      animationProps: {
+      name: 'random',
+          xMax: swarmElRect.right,
+          yMax: swarmElRect.bottom
+      }
+  });
+
+  // then freeze for another .1s
+  animationSequence.push({
+      duration: 100,
+      animationProps: {name: 'freeze'}
+  });
+
+  // then orbit back around the center
+  animationSequence.push({
+      animationProps: {
+          name: 'orbit',
+          orbitX: swarmElX,
+          orbitY: swarmElY,
+          radius: 50,
+          xMax: swarmElRect.right,
+          yMax: swarmElRect.bottom
+      }
+  });
+
+  cherrySwarmEl.animationProps = {
+      name: 'sequence',
+      animationSequence: animationSequence
+  };
+}
+
+$('#cherry-btn-about-me').click(() => onAboutMeBtnClick());
+$('#cherry-btn-experience').click(() => onExperienceBtnClick());
+$('#cherry-btn-projects').click(() => onProjectsBtnClick());
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+const PROD_CONFIG = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist')
@@ -11,5 +12,34 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
-  ]
+  ],
+  module:{
+    rules:[{
+      test: /\.html$/,
+      use: [
+        'html-loader'
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        'file-loader'
+      ]
+    }]
+  }
 };
+
+
+const DEV_CONFIG = Object.assign({}, PROD_CONFIG, {
+  mode: 'development',
+  devtool: 'inline-source-map'
+});
+
+module.exports = (env) => {
+  return (env === 'dev' ? DEV_CONFIG : PROD_CONFIG);
+}
